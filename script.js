56f0500af57d7e2f1bc3287961b7603700d71ac9
yousef271 <128397039+yousef271@users.noreply.github.com>
2025-04-01 11:53:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function widget() {
     const discord_wedgit = document.getElementById("discord_wedgit");
 
     // Check the current rotation and toggle
-    if (discord_wedgit.style.marginLeft === "-20%") {
+    if (discord_wedgit.style.marginLeft === "-80%") {
         discord_wedgit.style.marginLeft = "1%"; // Adjust according to the actual width
     } else {
-        discord_wedgit.style.marginLeft = "-20%"; // Move widget to the right
+        discord_wedgit.style.marginLeft = "-80%"; // Move widget to the right
     }
 }
